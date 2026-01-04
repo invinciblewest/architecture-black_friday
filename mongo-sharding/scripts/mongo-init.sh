49247fb09af79e64f931944b23d533b13ecc94e4
Владислав Борисенко <v.borisenko@notamedia.ru>
2026-01-04 17:52:46 +0500

    task 2 - sharding (ports)

--- mongo-sharding/scripts/mongo-init.sh
+++ mongo-sharding/scripts/mongo-init.sh
@@ -3,13 +3,13 @@
 ###
 # Инициализируем сервер конфигурации
 ###
-docker compose exec -T configSrv mongosh <<EOF
+docker compose exec -T configSrv mongosh --port 27019 <<EOF
 rs.initiate(
   {
     _id : "config_server",
     configsvr: true,
     members: [
-      { _id : 0, host : "configSrv:27017" }
+      { _id : 0, host : "configSrv:27019" }
     ]
   }
 );
@@ -18,12 +18,12 @@ EOF
 ###
 # Инициализируем shard1
 ###
-docker compose exec -T shard1 mongosh <<EOF
+docker compose exec -T shard1 mongosh --port 27018 <<EOF
 rs.initiate(
     {
       _id : "shard1",
       members: [
-        { _id : 0, host : "shard1:27017" },
+        { _id : 0, host : "shard1:27018" },
       ]
     }
 );
@@ -32,12 +32,12 @@ EOF
 ###
 # Инициализируем shard2
 ###
-docker compose exec -T shard2 mongosh <<EOF
+docker compose exec -T shard2 mongosh --port 27018 <<EOF
 rs.initiate(
     {
       _id : "shard2",
       members: [
-        { _id : 0, host : "shard2:27017" },
+        { _id : 0, host : "shard2:27018" },
       ]
     }
 );
@@ -56,8 +56,8 @@ done
 echo "mongos_router is healthy"
 
 docker compose exec -T mongos_router mongosh <<EOF
-sh.addShard( "shard1/shard1:27017");
-sh.addShard( "shard2/shard2:27017");
+sh.addShard( "shard1/shard1:27018");
+sh.addShard( "shard2/shard2:27018");
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
 use somedb
